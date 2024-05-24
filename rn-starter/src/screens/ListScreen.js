e937f10b4f684c90009e6361f86f6c806b91861e
DenysPashkov <30626869+DenysPashkov@users.noreply.github.com>
2024-05-24 15:38:26 +0200

    added the styles

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -4,26 +4,25 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 const ListScreenComponent = () => {
 
     const contacts = [
-        { name: "Mario" },
-        { name: "Marco" },
-        { name: "Maria" },
-        { name: "Miriam" },
-        { name: "Marcello" },
-        { name: "Marta" },
-        { name: "Mamma" },
-        { name: "Mariace festa" },
-        { name: "Mimmo" },
-        { name: "Merluzzo" },
-        { name: "Motorola" }
+        { name: "Mario", key: "1" },
+        { name: "Marco", key: "2" },
+        { name: "Maria", key: "3" },
+        { name: "Miriam", key: "4" },
+        { name: "Marcello", key: "5" },
+        { name: "Marta", key: "6" },
+        { name: "Mamma", key: "7" },
+        { name: "Mariace festa", key: "8" },
+        { name: "Mimmo", key: "9" },
+        { name: "Merluzzo", key: "10" },
+        { name: "Mario", key: "11" }
     ]
 
     function listRow(name) {
-        return <Text> - {name}</Text>
+        return <Text style={styles.cellStyle}> - {name}</Text>
     }
 
     function list() {
-        return <
-            FlatList 
+        return < FlatList 
             data={contacts} 
             renderItem={ ({item}) => {
                 return listRow(item.name)
@@ -34,9 +33,11 @@ const ListScreenComponent = () => {
     return list()
 }
 
-const syles = StyleSheet.create({
-    test: {
-        fontSize: 20
+const styles = StyleSheet.create({
+    cellStyle: {
+        backgroundColor: "red",
+        marginVertical: 50,
+        marginStart: 10
     }
 })
 
