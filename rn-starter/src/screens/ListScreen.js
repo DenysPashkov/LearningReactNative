47602926c6117e8ae45f86e3f4de76a56c9381e2
DenysPashkov <30626869+DenysPashkov@users.noreply.github.com>
2024-05-24 14:54:15 +0200

    added ListScreen.js for test lists

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { List, StyleSheet, Text } from 'react-native';
+
+const ListScreenComponent = () => {
+    return <Text>Hi</Text>
+}
+
+const syles = StyleSheet.create({
+    test: {
+        fontSize: 20
+    }
+})
+
+export default ListScreenComponent
