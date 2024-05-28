efc43391e63040962e4d078f6fa5bf6458c512a4
DenysPashkov <30626869+DenysPashkov@users.noreply.github.com>
2024-05-28 13:57:16 +0200

    Finished ImagesListScreen

--- rn-starter/src/components/ImagesCell.js
+++ rn-starter/src/components/ImagesCell.js
@@ -1,18 +1,38 @@
 import React from "react";
 import {View, Text, StyleSheet, Image } from 'react-native'
 
-function ImagesCell({title, img}) {
+function ImagesCell({title, img, score}) {
     return <View style={styles.cellStyle}>
-        <Image source={img}/>
-        <Text>{title}</Text>
+        <Image style={styles.imageStyle} source={img}/>
+        <View style={styles.textSectionStyle}>
+            <Text style={styles.imageName}>{title}</Text>
+            <Text style={styles.scoreText}>Score: {score}</Text>
+        </View>
     </View>
 }
 
 const styles = StyleSheet.create({
     cellStyle: {
         flexDirection: 'row',
-        alignItems: 'center',
-        gap: 20
+        alignItems: 'top',
+        gap: 10
+    },
+    imageStyle: {
+        borderRadius: 10,
+        width: 100,
+        height: 50
+    },
+    textSectionStyle: {
+        flexDirection: 'column'
+    },
+    imageName: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    scoreText: {
+        fontSize: 10,
+        fontWeight: 'thin',
+        color: 'gray'
     }
 })
 
--- rn-starter/src/screens/ImagesListScreen.js
+++ rn-starter/src/screens/ImagesListScreen.js
@@ -4,9 +4,9 @@ import ImagesCell from "../components/ImagesCell";
 
 const ImagesListScreen = () =>{
     return <View style={styles.listStyle}>
-        <ImagesCell title="beach" img={require("../../assets/beach.jpg")}/>
-        <ImagesCell title="forest" img={require("../../assets/forest.jpg")}/>
-        <ImagesCell title="mountain" img={require("../../assets/mountain.jpg")}/>
+        <ImagesCell title="beach" img={require("../../assets/beach.jpg")} score="3"/>
+        <ImagesCell title="forest" img={require("../../assets/forest.jpg")} score="5"/>
+        <ImagesCell title="mountain" img={require("../../assets/mountain.jpg")} score="6.5"/>
     </View>
 }
 
